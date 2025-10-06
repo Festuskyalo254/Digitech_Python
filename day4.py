98320bc888850dfb3ad775ace73a7c80e2692b2b
Festuskyalo254 <festusk625@gmail.com>
2025-10-06 12:11:59 +0300

    update day4

---
+++ day4.py
@@ -0,0 +1,13 @@
+#login validation in python
+print("Login window:")
+print()
+username=input("Enter Username:  ")
+password=input("Enter Password:  ")
+if username=="Allanwalker" and password=="g_finest1234#":
+    print("login successful")
+elif username!=username and password!=password:
+    print ("Wrong credentials")
+else:
+    print("username or password is incorrect")
+    
+
