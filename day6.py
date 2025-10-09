3b32874ec3e6fd9fcaf79939af1874ada3e224cd
Festuskyalo254 <festusk625@gmail.com>
2025-10-09 12:02:31 +0300

    day6 Bitwise operators

---
+++ day6.py
@@ -0,0 +1,21 @@
+#and in bitwise
+a=5
+b=4
+calc=a&b
+binary=bin(calc)
+print(f"{calc} and its binary is{binary}")
+
+#or in bitwise
+a=5
+b=4
+calc=a|b
+binary=bin(calc)
+print(f"{calc} and its binary is{binary}")
+#xor in bitwise
+a=5
+b=4
+calc=a^b
+binary=bin(calc)
+print(f"{calc} and its binary is{binary}")
+
+
